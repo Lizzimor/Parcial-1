839b191d897bf3e6c790f4f3e78ae797a6397109
Lizzimor <moronayalae@gmail.com>
2024-03-22 17:59:40 -0500

    Parcial1

---
+++ Login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "No hay campos", Toast.LENGTH_SHORT).show();
+                } else if (username.equals("uac123") || password.equals("12345678")) {
+                    Intent validado = new Intent(MainActivity.this, activity_login.class);
+                    startActivity(validado);
+                }else {
+                    Toast.makeText(MainActivity.this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
